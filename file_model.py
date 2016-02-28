c209b2932d1854d1370efdcefd1d3522035a05f4
BhuwaAn <BhuwaAn>
2016-02-28 23:54:33 +0530

    File model added-used for create folder and file

---
+++ file_model.py
@@ -0,0 +1,12 @@
+import os
+class FileModel:
+    
+    def create_dir_or_file(self,mypath):
+        if not os.path.isdir(mypath[ : mypath.rfind('/') ]):
+            os.makedirs(mypath[ : mypath.rfind('/') ])
+        if not os.path.exists(mypath):
+            f = file(mypath, "w+")
+    
+
+    
+    
