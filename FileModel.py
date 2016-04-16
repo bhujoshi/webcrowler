bb79269d278041055b99771defb827e0e071c3e4
BhuwaAn <BhuwaAn>
2016-04-16 23:50:52 +0530

    Html css and js files are downloading now

--- FileModel.py
+++ FileModel.py
@@ -1,10 +1,21 @@
 import os
 class FileModel:
+    path=''
     def create_dir_or_file(self,mypath):
+    #save the path for further use
+        self.path=mypath
         if not os.path.isdir(mypath[ : mypath.rfind('/') ]):
             os.makedirs(mypath[ : mypath.rfind('/') ])
         if not os.path.exists(mypath):
             f = file(mypath, "w+")
+    #write the current path        
+    def write_file(self,data):
+        if self.path == '' and data == '' :
+            print("Should be required file path and unable to write file with empty string")
+        else:
+            f = open(self.path, 'w+')
+            f.write(data)
+        
     
 
     
--- model.py
+++ model.py
@@ -2,19 +2,32 @@ import urllib2
 from FileModel import FileModel 
 class Model(FileModel):
     tags=['script','link','img']
+    str_files_path=[]
+    str_img_path=[]
     tags_positions=[]
     all_html_str=None
     #constructer funtion for class 
     def __init__(self,url):
         response = urllib2.urlopen(url)
         self.all_html_str = response.read()
+        self.create_dir_or_file('myproject/index.html')
+        self.write_file(self.all_html_str)
         for tag in self.tags :
             self.tags_positions.append(self.find_tag(tag))
-            
-        print self.tags_positions
-
+        print ("Downloading.................");    
+        #print self.tags_positions
         self.file_full_src()
-        
+        print(self.str_files_path)
+        for file_path in self.str_files_path:
+            full_path=url+'/'+file_path
+            url_obj = urllib2.urlopen(full_path)
+            all_html_str = url_obj.read()
+            #print (all_html_str)
+            self.create_dir_or_file('myproject/'+file_path)
+            self.write_file(all_html_str)
+            
+        print("your site is download sucessfully!");    
+            
         
     #check if tag is match 
     def check_tag_match( self, html_str, tag):
@@ -53,6 +66,8 @@ class Model(FileModel):
     def file_full_src(self):
         attr_in_tag_to_match=['src','href']
         url_string_start_index=None
+        #print(self.tags_positions);
+        count=0
         for tag_positions in self.tags_positions:
             for tag_index in tag_positions:
                 #print self.all_html_str[tag_index['start']:tag_index['end']+1]
@@ -70,12 +85,25 @@ class Model(FileModel):
                    
                     if url_string_start_index != None and self.check_tag_match('"',self.all_html_str[tag_index['start']:tag_index['start']+1]):
                         if self.all_html_str[url_string_start_index:tag_index['start']] != '':
-                            print self.all_html_str[url_string_start_index:tag_index['start']]
-                            tag_index['start'] = tag_index['end']
-                     
-                        
+                            my_file= self.all_html_str[url_string_start_index:tag_index['start']]
+                            if(count != 2):
+                                if self.check_http_file(my_file):
+                                    self.str_files_path.append(my_file)       
+                                
+                            else:
+                                if self.check_http_file(my_file):
+                                    self.str_img_path.append(my_file)       
+
+                            tag_index['start'] = tag_index['end'] 
                     tag_index['start']=tag_index['start']+1
+            count=count+1        
     
-                
+    def check_http_file(self,str):
+        if str[:4]=='http':
+            return False
+        else:
+            return True
+            
+        
     
 obj = Model('http://www.ezeeport.com')
